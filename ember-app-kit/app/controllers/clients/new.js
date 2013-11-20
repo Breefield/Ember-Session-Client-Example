fd1a667a617c62f50367a97e7e4a6a4b82327012
Dustin Hoffman <dustin.hoffman@breefield.com>
2013-11-19 20:25:33 -0800

    try and access model in controller to save

--- ember-app-kit/app/controllers/clients/new.js
+++ ember-app-kit/app/controllers/clients/new.js
@@ -1,6 +1,11 @@
 import ClientModel from 'appkit/models/client';
 
 var ClientsNewController = Ember.Controller.extend({
+  actions: {
+    save: function() {
+      console.log(this.get('model'));
+    }
+  }
 });
 
 export default ClientsNewController;
